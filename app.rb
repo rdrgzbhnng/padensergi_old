5349099f8505a87f0c6f0bf2b841ac21cd75607f
rodbeh <rodbeh@gmail.com>
2016-09-30 20:37:42 +0200

    Update images

--- app.rb
+++ app.rb
@@ -1,17 +1,5 @@
 require './boot.rb'
 
 get '/' do
-  erb :verkami
-end
-
-get '/castellano' do
-  erb :verkami
-end
-
-get '/verkami' do
-  erb :verkami
-end
-
-get '/anterior' do
-  erb :catala
+  erb :index
 end
